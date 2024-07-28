0a56485512ad23dbe2478b2bcfd04222bb92134a
karthik4dev <karthikpnrao.97@gmail.com>
2024-07-29 00:36:57 +0530

    changed to new number i.e, 300

--- src/Pokemon.js
+++ src/Pokemon.js
@@ -1,6 +1,6 @@
 import './styles/PokemonStyle.css'
 export default function PokemonAPI(){
-    const number=Math.floor(Math.random() * 100)+1;
+    const number=Math.floor(Math.random() * 300)+1;
     let url=`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${number}.png`;
     return(<div className="Pokemon">
         <h2>Pokemon {number}</h2><br></br>
