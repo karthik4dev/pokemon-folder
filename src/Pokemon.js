660c4c24a22274533ac0b1e99d27246d840275cf
karthik4dev <karthikpnrao.97@gmail.com>
2024-07-28 15:54:27 +0530

    Pokemon Project

---
+++ src/Pokemon.js
@@ -0,0 +1,10 @@
+import './styles/PokemonStyle.css'
+export default function PokemonAPI(){
+    const number=Math.floor(Math.random() * 100)+1;
+    let url=`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${number}.png`;
+    return(<div className="Pokemon">
+        <h2>Pokemon {number}</h2><br></br>
+        <img src={url} />
+        </div>
+        );
+}
