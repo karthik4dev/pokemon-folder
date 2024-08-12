cfccf5260cc39e37f3efa9d8a7c9b452483e981f
karthik4dev <karthikpnrao.97@gmail.com>
2024-08-13 00:00:12 +0530

    Modified packages

--- src/Pokemon.js
+++ src/Pokemon.js
@@ -1,10 +1,28 @@
+import axios from 'axios'
 import './styles/PokemonStyle.css'
+import { useState } from 'react';
 export default function PokemonAPI(){
-    const number=Math.floor(Math.random() * 300)+1;
-    let url=`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${number}.png`;
+    const number=Math.floor(Math.random() * 100)+1;
+    const [name,setName]= useState();
+    var height;
+    var weight;
+    let imageUrl=  `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${number}.png`
+    let altImageurl=`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${number}.png`
+    let url=`https://pokeapi.co/api/v2/pokemon/${number}/`;
+    const pokemon=axios.get(url).then((Response)=>{
+        let json= JSON.parse(Response.data);
+        console.log(json);
+    }).catch(function (e)
+    {
+        console.log(e);
+    })
+
     return(<div className="Pokemon">
         <h2>Pokemon {number}</h2><br></br>
-        <img src={url} />
+        <img src={imageUrl} alt={altImageurl} />
+        <h3>{name}</h3>
+        <h3>{height}</h3>
+        <h3>{weight}</h3>
         </div>
         );
 }
